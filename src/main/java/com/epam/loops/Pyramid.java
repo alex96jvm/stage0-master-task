5accf1fe53531aa1e2440cf7374be55f64b66479
alex96jvm <alex96jvm@gmail.com>
2024-08-31 01:12:11 +0300

    Complete task 2 Loops

--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -3,7 +3,24 @@ package com.epam.loops;
 public class Pyramid {
 
     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
 
+            StringBuilder line = new StringBuilder();
+
+            for (int j = cathetusLength; j > i; j--) {
+                line.append(" ");
+            }
+
+            for (int j = i; j > 0; j--) {
+                line.append(j);
+            }
+
+            for (int j = 2; j <= i; j++) {
+                line.append(j);
+            }
+
+            System.out.println(line);
+        }
     }
 
     public static void main(String[] args) {
