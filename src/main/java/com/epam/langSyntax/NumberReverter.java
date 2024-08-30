5e36f8a11cfe2570ae8b47b652ffae6232c43ecf
alex96jvm <alex96jvm@gmail.com>
2024-08-30 13:52:19 +0300

    Complete task 1 Lang Syntax

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,12 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        int revertedNumber = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            revertedNumber = revertedNumber * 10 + digit;
+            number /= 10;
+        }
+        System.out.println(revertedNumber);
     }
-
 }
