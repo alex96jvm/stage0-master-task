662e7e10a5d0d7b5ded02583a3ce5529a7302d1c
alex96jvm <alex96jvm@gmail.com>
2024-08-30 17:00:43 +0300

    Complete task 5 Conditions

--- src/main/java/com/epam/conditions/CoordinatePane.java
+++ src/main/java/com/epam/conditions/CoordinatePane.java
@@ -3,7 +3,21 @@ package com.epam.conditions;
 public class CoordinatePane {
 
     public void printQuadrant(int x, int y) {
+        String result;
+        if (x > 0 && y > 0){
+            result = "first";
+        } else if (x < 0 && y > 0) {
+            result = "second";
+        } else if (x < 0 && y < 0) {
+            result = "third";
+        } else if (x > 0 && y < 0) {
+            result = "fourth";
+        } else if (x == 0 && y == 0) {
+            result = "zero";
+        } else {
+            result = "on axis";
+        }
+        System.out.println(result);
     }
-
 }
 
