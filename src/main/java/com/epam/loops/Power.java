6d036e69f32c35f029f8e207d1f57c05e52b93a9
alex96jvm <alex96jvm@gmail.com>
2024-08-31 12:04:18 +0300

    Complete task 3 Loops

--- src/main/java/com/epam/loops/Power.java
+++ src/main/java/com/epam/loops/Power.java
@@ -4,6 +4,17 @@ public class Power {
 
     public void printPower(int numberToPrint, int power) {
 
+        int result = 1;
+
+        if (power < 0) {
+            System.out.println("Negative power of number are not supported");
+            return;
+        }
+
+        for (int i = 0; i < power; i++){
+            result *= numberToPrint;
+        }
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
