9f4bff0eebd3af93b209eaa41bd3c9263354b59d
alex96jvm <alex96jvm@gmail.com>
2024-08-31 13:21:43 +0300

    Complete task 4 Loops

--- src/main/java/com/epam/loops/HalfPyramid.java
+++ src/main/java/com/epam/loops/HalfPyramid.java
@@ -3,7 +3,12 @@ package com.epam.loops;
 public class HalfPyramid {
 
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            StringBuilder line = new StringBuilder();
+            line.append(" ".repeat(cathetusLength - i));
+            line.append("*".repeat(i));
+            System.out.println(line);
+        }
     }
 
     public static void main(String[] args) {
